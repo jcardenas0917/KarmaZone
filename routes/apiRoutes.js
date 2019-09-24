938501217f0432277e8d5d9c6c017660e5a78066
jaime cardenas <jcardenas@fixcityrepairs.com>
2019-09-23 21:27:33 -0400

    changed routes

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -2,21 +2,21 @@ var db = require("../models");
 
 module.exports = function(app) {
   // Get all examples
-  app.get("/api/examples", function(req, res) {
+  app.get("/api/cars", function(req, res) {
     db.Cars.findAll({}).then(function(dbCars) {
       res.json(dbCars);
     });
   });
 
   // Create a new example
-  app.post("/api/examples", function(req, res) {
+  app.post("/api/cars", function(req, res) {
     db.Cars.create(req.body).then(function(dbCars) {
       res.json(dbCars);
     });
   });
 
   // Delete an example by id
-  app.delete("/api/examples/:id", function(req, res) {
+  app.delete("/api/cars/:id", function(req, res) {
     db.Cars.destroy({ where: { id: req.params.id } }).then(function(dbCars) {
       res.json(dbCars);
     });
